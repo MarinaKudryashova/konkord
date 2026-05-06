3b4f1f2d9e808e46b7c6b84d11eced945a9d1cf5
Marina Kudryashova <milenacheb@mail.ru>
2026-05-06 21:06:28 +0300

    В промоблоке подтянула изображение с БД

--- template-parts/section/promo-main.php
+++ template-parts/section/promo-main.php
@@ -3,6 +3,8 @@
   
   $promo_title = get_field('promo_title', $page_id);
   $promo_descr = get_field('promo_descr', $page_id);
+  $promo_img_url = get_field('promo_bgimg', $page_id);
+  $promo_img = $promo_img_url ? get_image_versions($promo_img_url) : '#';
 ?>
 
 <section class="promo sec-offset sec-light">
@@ -17,10 +19,12 @@
       <p class="promo__descr"><?php echo esc_html($promo_descr); ?></p>
       <?php endif; ?>
       
-      <!-- <picture>
-        <source srcset=".webp" type="image/webp">
-        <img loading="lazy" src="." class="image" width="" height="" alt="">
-      </picture> -->
+      <picture>
+        <?php if($promo_img["webp_1x"]) : ?>
+          <source srcset="<?php echo esc_url($promo_img["webp_1x"]); ?>" type="image/webp">
+        <?php endif; ?>
+        <img src="<?php echo esc_url($promo_img["original_1x"]); ?>" class="image" width="651" height="545" alt="">
+      </picture>
 
       <?php /*-- Кнопка с формой --*/ ?>
 			<button type="button" class="promo__callback ui-btn" data-graph-path="modal-leadform">Узнать стоимость</button>
