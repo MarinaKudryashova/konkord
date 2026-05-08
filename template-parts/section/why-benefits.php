168126192f636bbcfe710d057861fd1789f6182a
Marina Kudryashova <milenacheb@mail.ru>
2026-05-08 19:55:19 +0300

    изменение разметки why-benefits + стили десктоп

--- template-parts/section/why-benefits.php
+++ template-parts/section/why-benefits.php
@@ -11,28 +11,39 @@
 
 ?>
 <section class="why-benefits <?php if($sec_is_last != 1) : ?>sec-offset"<?php endif; ?>>
-  <div class="container">
-    <?php if($sec_why_title) : ?>
-      <h2 class="sec-title" data-aos="fade-up"><?php echo $sec_why_title; ?></h2>
-    <?php endif; ?>
+  <div class="why-benefits__container container">
+    <!-- <?php //if($sec_why_title) : ?>
+      <h2 class="sec-title" data-aos="fade-up"><?php //echo $sec_why_title; ?></h2>
+    <?php //endif; ?> -->
 
     <?php if(!empty($sec_why_list) && is_array($sec_why_list)) : ?>
     <ul class="why-benefits__list">
-      <?php foreach($sec_why_list as $benefit) : 
+      <?php foreach($sec_why_list as $ids => $benefit) : 
         $benefit_title = $benefit["title"];
         $benefit_text = $benefit["text"];
+
+        $total_items = count($sec_why_list);
+        $item_type = match(true) {
+            $ids == 0 => 'first',
+            $ids == $total_items - 1 => 'last',
+            default => 'middle'
+        };
         ?>
         <li class="why-benefits__item">
+          <?php if($item_type === 'first' && $sec_why_title) : ?>
+            <h2 class="sec-title" data-aos="fade-up"><?php echo $sec_why_title; ?></h2>
+          <?php endif; ?>
           <div class="card-benefits">
             <span class="card-benefits__name"><?php echo wp_kses_post($benefit_title); ?></span>
             <span class="card-benefits__text"><?php echo wp_kses_post($benefit_text); ?></span>
           </div>
+          <?php if($item_type === 'last') : ?>
+            <?php /*-- Кнопка с формой --*/ ?>
+            <a href="#sec-employees" class="why-benefits__link ui-btn">Связаться с менеджером</a>
+          <?php endif; ?>
         </li>
       <?php endforeach; ?>
     </ul>
     <?php endif; ?>
-    
-    <?php /*-- Кнопка с формой --*/ ?>
-    <button type="button" class="promo__callback ui-btn" data-graph-path="modal-leadform">Связаться с менеджером</button>
   </div>
 </section>
