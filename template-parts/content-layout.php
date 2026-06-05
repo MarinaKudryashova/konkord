b3b17f9dfc89f1fb79e325e9e2eedee2c758552f
SvetaVarghese <svetlanavarghese@gmail.com>
2026-06-05 19:59:19 +0300

    add layout, about sections for desktop, add breadcrumbs, fix search

--- template-parts/content-layout.php
+++ template-parts/content-layout.php
@@ -1,1 +1,321 @@
-Здесь твой код для страницы Требования к макетам
+<section class="layout sec-offset">
+   <div class="layout__container container">
+      <h2 class="layout__title sec-title">Требования к макетам</h2>
+      <div class="layout__requirements">
+         <h3 class="layout__subtitle">Общие требования к принимаемым макетам</h3>
+         <ul class="layout__list">
+            <li class="layout__item">
+               <p class="layout__text">
+                  Оригинал-макет принимается в программе Corel Draw 16, Adobe Photoshop, формат: <b>ai,  tiff,  pdf,  cdr, jpg</b> 
+                  (не менее 300 dpi), <b>eps</b> (в кривых), <b>psd</b> (со слитыми слоями)
+               </p>
+            </li>
+            <li class="layout__item">
+               <p class="layout__text">
+                  Макеты в программах <b>Word, Exell, PowerPoint</b> и других, не предназначенных для вёрстки,не принимаются 
+                  (в них допускается принятие информации, тех. задание, черновая вёрстка).
+               </p>
+            </li>
+            <li class="layout__item">
+               <p class="layout__text">
+                  Все <b>шрифты</b> должны быть переведены в <b>кривые</b>.
+               </p>
+            </li>
+            <li class="layout__item">
+               <p class="layout__text">
+                  В макете <b>не должны</b> присутствовать прозрачности, эффекты, объекты Power Clip.
+               </p>
+            </li>
+            <li class="layout__item">
+               <p class="layout__text">
+                  Убедитесь, что все RGB изображения, графические элементы и текст переведены <b>в цвета CMYK</b>.
+               </p>
+            </li>
+            <li class="layout__item">
+               <p class="layout__text">
+                  Максимальная суммарная красочность макета (%Cyan+%Magenta+%Yellow+%Black) для печати на мелованных <b>глянцевых</b> бумагах — <b>320%</b>, 
+                  мелованных <b>матовых</b> бумагах — <b>300%</b>, на <b>немелованных</b> бумагах, <b>картоне — 280%</b>. Не принимаются файлы, 
+                  где используется общее количество краски, большее, <b>чем 300–320 процентов</b> (параметр Total ink)
+               </p>
+            </li>
+         </ul>
+      </div>
+      <div class="layout__services layout-catalog">
+         <div class="layout-catalog__heading">
+            <h3 class="layout-catalog__title layout__subtitle">Офсетная печать</h3>
+            <span class="layout-catalog__type">Общие требования</span>
+         </div>
+         <ul class="layout-catalog__list">
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Технические требования</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+         </ul>
+      </div>
+      <div class="layout__services layout-catalog">
+         <div class="layout-catalog__heading">
+            <h3 class="layout-catalog__title layout__subtitle">Типографские услуги</h3>
+            <span class="layout-catalog__type">Примеры изделий</span>
+         </div>
+         <ul class="layout-catalog__list">
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Визитка (90х50 мм)</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Листовка (ф. А5)</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Буклет ф. А4 в 2 сложения</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Буклет ф. А4 в 3 сложения</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Каталог ф.А3</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Этикетки</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Фигурные этикетки</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+         </ul>
+      </div>
+      <div class="layout__services layout-catalog">
+         <div class="layout-catalog__heading">
+            <h3 class="layout-catalog__title layout__subtitle">Календарная продукция</h3>
+         </div>
+         <ul class="layout-catalog__list">
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Карманный календарь 70*100мм</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Квартальный БИЗНЕС ф. А4 (3 пружины, 3 рекламных поля)</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Квартальный БИЗНЕС ф. А4 (3 пружины, 1 рекламное поле)</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Квартальный VIP ф. А4 (3 пружины, 3 рекламных поля)</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Квартальный MINI ф. А4 (1 пружина, 1 рекламное поле)</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Домик ф. А4</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Домик с перекидными листами ф. А4</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Карманный календарь (100х70 мм)</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Настенный ф.А3</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Настенный перекидной ф.А3</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+         </ul>
+      </div>
+      <div class="layout__services layout-catalog">
+         <div class="layout-catalog__heading">
+            <h3 class="layout-catalog__title layout__subtitle">Штемпельная продукция / Лазерная гравировка</h3>
+         </div>
+         <ul class="layout-catalog__list">
+            <li class="layout-catalog__item">
+               <span class="layout-catalog__count"></span>
+               <span class="layout-catalog__file">
+                  <img loading="lazy" src="<?php echo get_template_directory_uri();?>/img/icon/pdf-file.svg" width="24" height="24" alt="" aria-hidden="true">
+                  <span>Печати, штампы, факсимиле</span>
+               </span>
+               <a href="" class="layout-catalog__link">
+                  <svg>
+							<use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#download"></use>
+						</svg>
+                  <span>Скачать</span>
+               </a>
+            </li>
+         </ul>
+      </div>
+   </div>
+</section>
