bd2e062aa1b107548061c6179b5eeb0f94e0ab91
SvetaVarghese <svetlanavarghese@gmail.com>
2026-06-10 22:48:47 +0300

    styles for cookie-notice, adaptive for sec-contacts

--- template-parts/components/cookie-notice.php
+++ template-parts/components/cookie-notice.php
@@ -18,6 +18,6 @@
 
       <div class="cookie-notice__text"><?php echo wp_kses( get_field('cookie_text', 'option'), $allowed_html); ?></div>
     <?php endif; ?>
-    <button id="cookie-accept" class="cookie-notice__btn ui-btn ui-btn--blue"><?php if(!empty(get_field('cookie_btn_text', 'option'))) : echo esc_html(get_field('cookie_btn_text', 'option')); else : echo esc_html('Согласен'); endif; ?></button>
+    <button id="cookie-accept" class="cookie-notice__btn ui-btn"><?php if(!empty(get_field('cookie_btn_text', 'option'))) : echo esc_html(get_field('cookie_btn_text', 'option')); else : echo esc_html('Согласен'); endif; ?></button>
   </div>
 </div>
