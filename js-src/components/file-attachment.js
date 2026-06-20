216a89b57c83eb363a09dc9b9c4baf6053c32bbb
Marina Kudryashova <milenacheb@mail.ru>
2026-06-20 20:31:16 +0300

    merge

--- js-src/components/file-attachment.js
+++ js-src/components/file-attachment.js
@@ -1,14 +1,14 @@
 const fileInput = document.getElementById("file-attachment");
-const fileLabel = document.querySelector('label[for="file-attachment');
+const fileLabel = document.querySelector('label[for="file-attachment"]');
 
 const defaultText = fileLabel.textContent;
 
-document.addEventListener('DOMContentLoaded', () => {
-   fileInput.addEventListener('change', () => {
-     if(fileInput.files.length > 0) {
+document.addEventListener("DOMContentLoaded", () => {
+  fileInput.addEventListener("change", () => {
+    if (fileInput.files.length > 0) {
       fileLabel.textContent = fileInput.files[0].name;
-     } else {
+    } else {
       fileLabel.textContent = defaultText;
-     }
-   })
-})
+    }
+  });
+});
